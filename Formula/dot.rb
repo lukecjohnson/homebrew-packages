b1bbbd5b52991159487285eda61343b5e6eded49
Luke Johnson <luke.johnson9423@gmail.com>
2020-04-26 12:10:18 -0400

    release: dot 1.0.0-beta.2

--- Formula/dot.rb
+++ Formula/dot.rb
@@ -1,10 +1,10 @@
 class Dot < Formula
   desc "A simple tool to save and run frequently used commands"
   homepage "https://github.com/lukecjohnson/dot"
-  version "1.0.0-beta.1"
+  version "1.0.0-beta.2"
   bottle :unneeded
-  url "https://github.com/lukecjohnson/dot/releases/download/1.0.0-beta.1/dot-1.0.0-beta.1-macos-amd64.tar.gz"
-  sha256 "38c7978359fe8d762abe043edd189a980c8576f63d1bb5606cc13c3bd0bcf37a"
+  url "https://github.com/lukecjohnson/dot/releases/download/1.0.0-beta.2/dot-1.0.0-beta.2-macos-amd64.tar.gz"
+  sha256 "bdbbe086f0726fd7f80bb36791e6430a63973fc9bef2612802206a0d9ea46d7d"
 
   def install
     bin.install "./bin/dot"
