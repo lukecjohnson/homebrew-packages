90ca8702e1cb8a643b47e855cbd757d5f99c3639
Luke Johnson <luke.johnson9423@gmail.com>
2020-04-30 18:06:25 -0400

    release: run 1.0.0-beta.3
    release: run 1.0.0-beta.3

--- Formula/run.rb
+++ Formula/run.rb
@@ -1,10 +1,10 @@
 class Run < Formula
   desc "A simple tool to save and run frequently used commands"
   homepage "https://github.com/lukecjohnson/run"
-  version "1.0.0-beta.2"
+  version "1.0.0-beta.3"
   bottle :unneeded
-  url "https://github.com/lukecjohnson/run/releases/download/1.0.0-beta.2/run-1.0.0-beta.2-macos-amd64.tar.gz"
-  sha256 "051d17fafe3843a056e43b90d5fca428eaab0351163ca1eef4f48307103c3ca4"
+  url "https://github.com/lukecjohnson/run/releases/download/1.0.0-beta.3/run-1.0.0-beta.3-macos-amd64.tar.gz"
+  sha256 "f3deda751f1a05bd7969b66eae59993f85f555c841884b8a609696818d9c10e1"
 
   def install
     bin.install "./bin/run"
