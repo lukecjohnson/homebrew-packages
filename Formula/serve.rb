7e30973339d20e1e2b6bd473d0ad14f49d39b8a8
Luke Johnson <luke.johnson9423@gmail.com>
2020-07-12 18:49:39 -0400

    release: serve 1.0.2

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -1,10 +1,10 @@
 class Serve < Formula
   desc "A quick, simple CLI for serving static sites and single page applications"
   homepage "https://github.com/lukecjohnson/serve"
-  version "1.0.1"
+  version "1.0.2"
   bottle :unneeded
-  url "https://github.com/lukecjohnson/serve/releases/download/1.0.1/serve-1.0.1-macos-64.tar.gz"
-  sha256 "9d40c26704e61d1b25eccc32e50bc881fa4bb7367182e072ab8db7b0372875ad"
+  url "https://github.com/lukecjohnson/serve/releases/download/1.0.2/serve-1.0.2-macos-64.tar.gz"
+  sha256 "747f173f96fb3c0925265e4fc892f6a859d9de1b2ff53f1caaedf609768acec2"
 
   def install
     bin.install "./bin/serve"
