2c563d6b9f375d1556bf4a1e0bfb6e145442fd0b
Luke Johnson <luke.johnson9423@gmail.com>
2021-04-15 17:07:33 -0400

    publish toolkit 1.1.0

--- Formula/toolkit.rb
+++ Formula/toolkit.rb
@@ -1,15 +1,15 @@
 class Toolkit < Formula
   desc "A collection of handy CLI tools"
   homepage "https://github.com/lukecjohnson/toolkit"
-  version "1.0.0"
+  version "1.1.0"
   bottle :unneeded
 
   if Hardware::CPU.arm?
-    url "https://github.com/lukecjohnson/toolkit/releases/download/1.0.0/toolkit-1.0.0-macos-arm64.tar.gz"
-    sha256 "bbb32e72749384f071123eb57917dfdb8e1f8f512442de9975f7852e341ae650"
+    url "https://github.com/lukecjohnson/toolkit/releases/download/1.1.0/toolkit-1.1.0-macos-arm64.tar.gz"
+    sha256 "bd793b47d4abcae20b1563e99ca6df040eb457438bd8a4ac9299b7e98e889526"
   else
-    url "https://github.com/lukecjohnson/toolkit/releases/download/1.0.0/toolkit-1.0.0-macos-amd64.tar.gz"
-    sha256 "5c6e199d606fd27cccaf4e32a129743a2773ba806a7c2d510f323a0a4a3afbb1"
+    url "https://github.com/lukecjohnson/toolkit/releases/download/1.1.0/toolkit-1.1.0-macos-amd64.tar.gz"
+    sha256 "880af53204ff582569d624bccf9ca5e5e704b65140f144818c8c688c31f6015f"
   end
 
   def install
